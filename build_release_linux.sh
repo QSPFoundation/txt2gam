4bb0d266c7a174e5c951fbcb5f132ade2b2a7702
Varg <va@pm.me>
2024-11-01 00:47:41 +0100

    Use separate Win/Linux/MacOS build jobs

--- build_release_linux.sh
+++ build_release_linux.sh
@@ -4,8 +4,5 @@ export RELEASE_VER="${1:-0.0.0}"
 
 mkdir -p dist
 
-echo "Preparing Win32 release $RELEASE_VER"
-./build_packages/pack_win32.sh
-
 echo "Preparing Linux64 release $RELEASE_VER"
 ./build_packages/pack_linux64.sh
