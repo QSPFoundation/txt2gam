3036797259cd9d6e9acfe0a9e9227d2d0a3065d6
Varg <va@pm.me>
2026-06-06 02:15:00 +0200

    Add missing pack_wasm.sh

---
+++ build_packages/pack_wasm.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+# Validation
+[ ! -d "./build_packages" ] && echo "Run this script from the project root directory" && exit
+[ -z "$RELEASE_VER" ] && echo "RELEASE_VER isn't specified" && exit
+
+CMAKE_VER=$(echo "$RELEASE_VER" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+
+WASM_BUILD_DIR=./build_packages/wasm
+
+mkdir -p $WASM_BUILD_DIR/packages
+
+emcmake cmake -S . -B $WASM_BUILD_DIR \
+    -DAPP_VERSION="$CMAKE_VER" \
+    -DCPACK_OUTPUT_FILE_PREFIX=$WASM_BUILD_DIR/packages \
+    -DCPACK_GENERATOR="ZIP" \
+    -DCMAKE_BUILD_TYPE=Release
+
+emmake make -C $WASM_BUILD_DIR --jobs=$(nproc)
+cpack -B $WASM_BUILD_DIR --config $WASM_BUILD_DIR/CPackConfig.cmake
+
+mkdir -p ./dist
+cp $WASM_BUILD_DIR/packages/*.zip "./dist/txt2gam-$RELEASE_VER-wasm.zip"
+( cd ./dist && sha256sum "txt2gam-$RELEASE_VER-wasm.zip" > "txt2gam-$RELEASE_VER-wasm.zip.sha256" )
